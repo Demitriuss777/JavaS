7c3ac554c568cfc72aec9a022f2e57ee2bef08a7
Demitriuss777 <demitriusss777@gmail.com>
2022-11-08 23:57:11 +0200

    lesson3

---
+++ JS/lesson/lesson3/if/main.js
@@ -0,0 +1,72 @@
+/*
+let x = 100;
+console.log(x);
+/!*confirm(abc);*!/
+let con = confirm('how old are you');
+console.log(con);
+/!*
+document.write('dsdsdds');*!/
+if (con){
+    document.write('film');
+} else {
+    document.write('cartoon');
+}*/
+/*
+let age = prompt( 'how old are you');
+if(age <18){
+    document.write('<h1>cartoon</h1>');
+}
+else if (age >= 18 && age < 90) {
+    document.write('<h1>corn hub</h1>');
+}
+else if (age>=90){
+    document.write('santa Barbara');
+}
+else {
+    document.write('<h1> ???? </h1>')
+}*/
+// let color = prompt('enter color that you see')
+// let roadstatus = confirm('road clean?')
+// // if (color=== 'green'){
+// //     if (roadstatus){
+// //     console.log('you can go');
+// // } }
+// // if (color === 'green' && roadstatus)
+//     if (color === 'green' || roadstatus)
+// { console.log('you can go');
+// }
+// else if (color ==='yelow'){
+//     console.log('wait');
+// }
+// else if ( color === ' red') {
+//     console.log(stop);
+// }
+// else {
+//     console.log('problem');
+// }
+
+// let x = confirm('what?');
+// if (x=== true) {
+//     x = 110;
+// } else {
+//     x = 100;
+// }
+// console.log(x);
+/*
+let x = confirm('what?')? 100: 90
+// console.log(x);*/
+switch ('yelov') {
+    case 'green':
+        console.log('go');
+        break;
+    case 'yelov':
+        console.log('wait');
+        break;
+    case 'red':
+        console.log('stop');
+        break;
+    default:
+        console.log('???');
+
+}
+
